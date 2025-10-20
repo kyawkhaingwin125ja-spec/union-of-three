e3aede26746b8213298040ea7ae3ac3063d76304
kyawkhaingwin <kyawkhaingwin125ja@gmail.com>
2025-10-20 08:50:21 +0630

    integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,27 @@
+// import { Stack } from "expo-router";
+
+// export default function RootLayout() {
+//   return (
+//     <Stack
+//       screenOptions={{
+//         headerShown: false, // 👈 hides the header for all screens
+//       }}
+//     />
+//   );
+// }
+
+// app/index.tsx
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false, // 👈 hides the header for all screens
-      }}
-    />
+    <Stack screenOptions={{ headerShown: false }} initialRouteName="password">
+      {/* The Slot or Stack.Screen shows your current route */}
+      <Stack.Screen name="index" />
+      <Stack.Screen name="password" />
+      <Stack.Screen name="password_typing"/>
+    </Stack>
   );
 }
+
+
